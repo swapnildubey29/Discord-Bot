9cba59abd86a7f869370f8509a22378da2f513ef
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-17 12:41:38 +0530

    Initialise Project

---
+++ main/src/bot.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, 
+    GatewayIntentBits.GuildMembers, 
+  ],
+});
+
+client.on
+
+
+client.login(process.env.DISCORD_BOT_TOKEN)
+  .then(() => console.log('Bot is logged in!'))
+  .catch(console.error)
